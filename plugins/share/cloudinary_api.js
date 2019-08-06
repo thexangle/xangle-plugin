6970f4ab655e6ae73f543b6865c38d30f67c9519
Laurent Siroen <laurent@xangle.co>
2019-08-06 11:46:04 -0400

    Better progress report

--- plugins/share/cloudinary_api.js
+++ plugins/share/cloudinary_api.js
@@ -34,21 +34,22 @@ cloudinaryModule.share = function(request){
     global.logger.debug("[PLLUGIN] Received cloudinary share request: ", request.timestamp);
     //create a list of tasks to run in parallel using async module
     var parallel_tasks = [];
-    
     if(request.timestamp && request.published_content && request.published_content.original_files){
-        var targetFolder = request.folder ? request.folder : request.timestamp;
         request.published_content.original_files.forEach((entry) => {
             if(entry.url && request.root_path) {
                 parallel_tasks.push( (callback) => {
                     // First notify Xangle that we are starting to upload a file. 
-                    cloudinaryModule.update(request.timestamp, entry.url, {status: "uploading",  camera_index: entry.camera_index }, () => {
+                    cloudinaryModule.update(request.timestamp, entry.url, {status: "uploading", camera_index: entry.camera_index}, () => {
                         // Upon receiving response from Xangle, proceed to upload on cloudinary
-                        cloudinary.uploader.upload(request.root_path + entry.url, {folder: targetFolder}, (err, response) => {
+                        cloudinary.uploader.upload(request.root_path + entry.url, {folder: request.timestamp}, (err, response) => {
                             //console.log("cloudinary response: ", err ? "ERROR:" + JSON.stringify(err, null, 4) : JSON.stringify(response, null, 4));
                             // Update upload status on server with the info returned by cloudinary
                             let update_info =  {status: err ? "failed" : "uploaded", camera_index: entry.camera_index, result: response};
+                            if(err){
+                                update_info.error = err;
+                            }
                             cloudinaryModule.update(request.timestamp, entry.url, update_info, () => {
-                                return callback(null, err ? err: null)
+                                return callback(null, update_info)
                             });
                         });
                     });
@@ -61,14 +62,12 @@ cloudinaryModule.share = function(request){
     async.parallel(parallel_tasks, (err, results) =>{
         //If [results] array has some values then it means some tasks failed. 
         var success = !err;
-        var errors = [];
         results.forEach( (res) => {
-            if(res != null){
+            if(res.error != null){
                 success = false;
             }
-            errors.push(res);
         });
         // And notify the server when all the tasks are finished
-        cloudinaryModule.update(request.timestamp, null, {status: success ? "uploaded" : "failed", errors: errors});
+        cloudinaryModule.update(request.timestamp, null, {status: success ? "uploaded" : "failed", results: results});
     })
 }
